e68451a3e9085283d5707cdfdd8c4c2ab4eaee55
Kevin Garzon <kevin.garzon@mail.escuelaing.edu.co>
2021-08-24 22:10:53 -0500

    Modificado

--- src/main/java/edu/escuelaing/WebApp/HttpConnectionExample.java
+++ src/main/java/edu/escuelaing/WebApp/HttpConnectionExample.java
@@ -12,11 +12,13 @@ public class HttpConnectionExample {
     private static final String USER_AGENT = "Mozilla/5.0";
     //private static final String GET_URL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=fb&apikey=Q1QZFVJQ21K7C6XM";
     private final String GET_URL = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=";
+    private final String GET_URL2 = "https://cloud.iexapis.com/stable/stock/market/batch/time_series?symbols=";
     private final String API_KEY = "V9G35RET2PICR1YC";
-    private String res;
+    private final String TOKEN = "pk_c283a36c3e64499e809005a0f4e28bb9";
 
-    public String getHtmlResponse(String id) throws IOException {
+    public String getAlphavantageResponse(String id) throws IOException {
 
+        String res;
         URL obj = new URL(GET_URL + id + "&apikey=" + API_KEY);
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
         con.setRequestMethod("GET");
@@ -24,7 +26,39 @@ public class HttpConnectionExample {
 
         //The following invocation perform the connection implicitly before getting the code
         int responseCode = con.getResponseCode();
-        System.out.println("GET Response Code :: " + responseCode);
+        //System.out.println("GET Response Code :: " + responseCode);
+
+        if (responseCode == HttpURLConnection.HTTP_OK) { // success
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            // print result
+            res = response.toString();
+        } else {
+            res = "GET request not worked";
+        }
+
+        return res;
+    }
+
+    public String getIextradingResponse(String id) throws IOException {
+
+        String res;
+        URL obj = new URL(GET_URL2 + id + "&types=quote,chart&range=" + API_KEY + "&token=pk_04b714a99e6d4d6394657768c1274360");
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("GET");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        //The following invocation perform the connection implicitly before getting the code
+        int responseCode = con.getResponseCode();
+        //System.out.println("GET Response Code :: " + responseCode);
 
         if (responseCode == HttpURLConnection.HTTP_OK) { // success
             BufferedReader in = new BufferedReader(new InputStreamReader(
--- src/main/java/edu/escuelaing/WebApp/Main.java
+++ src/main/java/edu/escuelaing/WebApp/Main.java
@@ -5,42 +5,68 @@ import spark.Response;
 import java.io.IOException;
 import static spark.Spark.*;
 import edu.escuelaing.WebApp.HttpConnectionExample;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class Main {
+
+  private static ConcurrentHashMap<String, String> alphavantageCache = new ConcurrentHashMap<>();
+  private static ConcurrentHashMap<String, String> iextradingCache = new ConcurrentHashMap<>();
+
+
   public static void main(String[] args) {
     port(getPort());
     staticFiles.location("/public");
-    get("/form", (req, res) -> formPage(req, res));
-    get("/results", (req, res) -> resultPage(req, res));
-    get("/shares", (req, res) -> resultPage(req, res));
+    get("/alphavantage", (req, res) -> alphavantagePage(req, res));
+    get("/iextrading", (req, res) -> iextradingPage(req, res));
   }
 
-  private static String formPage(Request req, Response res) {
-    String pageContent = "<!DOCTYPE html>" +
-                         "<html>" +
-                         "<body>" +
-                         "<h2>Form</h2>" +
-                         "<form action=\"/results\">" +
-                         "  First name:<br>" +
-                         "  <input type=\"text\" name=\"firstname\" value=\"Mickey\">" +
-                         "  <br>" +
-                         "  Last name:<br>" +
-                         "  <input type=\"text\" name=\"lastname\" value=\"Mouse\">" + "  <br><br>" +
-                         "  <input type=\"submit\" value=\"Submit\">" +
-                         "</form>" +
-                         "<p>If you click the \"Submit\" button, the form-data will be sent to a page called \"/results\".</p>" +
-                         "</body>" +
-                         "</html>";
-    return pageContent;
-  }
+  private static JSONObject alphavantagePage(Request req, Response res) throws ParseException {
 
-  private static String resultPage(Request req, Response res) throws IOException {
+    String result = "None";
+    String id = req.queryParams("id");
     HttpConnectionExample connection = new HttpConnectionExample();
-    return connection.getHtmlResponse(req.queryParams("id"));
+
+    if (!alphavantageCache.containsKey(id)) {
+      try {
+        result = connection.getAlphavantageResponse(id);
+        alphavantageCache.put(id, result);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+
+    result = alphavantageCache.get(id);
+    JSONParser parser = new JSONParser();
+    JSONObject json = (JSONObject) parser.parse(result);
+    return json;
   }
 
-  private static int getPort() {
-    if (System.getenv("PORT") != null) {
+  private static String iextradingPage(Request req, Response res) {
+
+    String result = "None";
+    String id = req.queryParams("id");
+    HttpConnectionExample connection = new HttpConnectionExample();
+
+    if (!iextradingCache.containsKey(id)) {
+      try {
+        result = connection.getIextradingResponse(id);
+        iextradingCache.put(id, result);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+    result = iextradingCache.get(id);
+    return result;
+  }
+
+  private static int getPort()
+  {
+    if (System.getenv("PORT") != null)
+    {
       return Integer.parseInt(System.getenv("PORT"));
     }
     return 4567;
